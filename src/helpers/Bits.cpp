e03a8b808d0d9bb83ed73de21143ded799de34da
SuviTR <36501081+SuviTR@users.noreply.github.com>
2022-10-17 12:10:14 +0300

    Bit table

---
+++ src/helpers/Bits.cpp
@@ -0,0 +1,26 @@
+
+/**
+ * Digit	Bit Equivalent
+ * 0	    0000
+ * 1	    0001
+ * 2	    0010
+ * 3	    0011
+ * 4	    0100
+ * 5	    0101
+ * 6	    0110
+ * 7	    0111
+ * 8	    1000
+ * 9	    1001
+ * A	    1010
+ * B	    1011
+ * C	    1100
+ * D	    1101
+ * E	    1110
+ * F	    1111
+
+
+ * unsigned char zero = 0x00;    // 0000 0000
+ * unsigned char one = 0x01;     // 0000 0001
+ * unsigned char allOnes = 0xFF; // 1111 1111
+ * unsiged char zebra = 0x55;    // 0101 0101
+ */
